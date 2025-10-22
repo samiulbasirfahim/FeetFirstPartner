828136f01bc2030e250d1223ddfb588520eaff16
samiulbasirfahim <samiulbasirfahim.rxen@gmail.com>
2025-10-22 15:39:17 +0600

    login & customer status updated

--- app/login.tsx
+++ app/login.tsx
@@ -9,62 +9,74 @@ import { colors } from "@/constants/colors";
 import { router } from "expo-router";
 import { StyleSheet, View } from "react-native";
 import { useAuthStore } from "@/store/auth";
+import { useState } from "react";
+import { LoginPayload } from "@/types/auth";
 
 export default function LoginScreen() {
-    const { login } = useAuthStore();
-    const handleSignIn = () => {
-        login();
-        router.replace("/(tabs)/scan");
-    };
+  const { login } = useAuthStore();
 
-    return (
-        <RNSafeAreaView>
-            <RNKeyboardAwareScrollView>
-                <RNText
-                    variant="title"
-                    style={{
-                        textAlign: "center",
-                        fontWeight: "600",
-                        fontSize: 28,
-                        marginTop: 32,
-                    }}
-                >
-                    Login
-                </RNText>
-                <View>
-                    <View style={styles.form}>
-                        <Image
-                            source={logo}
-                            style={{
-                                width: 80,
-                                height: 80,
-                                alignSelf: "center",
-                                marginTop: 16,
-                                marginBottom: 32,
-                            }}
-                        />
-                        <RNInput label="Benutzername or E-Mail" />
-                        <RNInput label="Passwort" secureTextEntry />
-                        <RNButton
-                            onPress={handleSignIn}
-                            label="Anmelden"
-                            variant="primary"
-                            style={{ marginTop: 16 }}
-                        />
-                    </View>
-                </View>
-            </RNKeyboardAwareScrollView>
-        </RNSafeAreaView>
-    );
+  const [form, setForm] = useState<LoginPayload>({
+    email: "",
+    password: "",
+  });
+
+  const [err, setErr] = useState<Partial<LoginPayload>>({});
+
+  const handleSignIn = () => {
+    login(form);
+    router.replace("/(tabs)/scan");
+  };
+
+  return (
+    <RNSafeAreaView>
+      <RNKeyboardAwareScrollView>
+        <RNText
+          variant="title"
+          style={{
+            textAlign: "center",
+            fontWeight: "600",
+            fontSize: 28,
+            marginTop: 32,
+          }}
+        >
+          Login
+        </RNText>
+        <View>
+          <View style={styles.form}>
+            <Image
+              source={logo}
+              style={{
+                width: 80,
+                height: 80,
+                alignSelf: "center",
+                marginTop: 16,
+                marginBottom: 32,
+              }}
+            />
+            <RNInput label="Benutzername or E-Mail" />
+            {err.email && <RNText variant="error">{err.email}</RNText>}
+            <RNInput label="Passwort" secureTextEntry />
+            {err.email && <RNText variant="error">{err.password}</RNText>}
+            <RNButton
+              onPress={handleSignIn}
+              label="Anmelden"
+              variant="primary"
+              style={{ marginTop: 16 }}
+            />
+          </View>
+        </View>
+      </RNKeyboardAwareScrollView>
+    </RNSafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    form: {
-        padding: 16,
-        borderRadius: 8,
-        borderWidth: 1,
-        borderColor: colors.border,
-        marginTop: 24,
-        gap: 8,
-    },
+  form: {
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: colors.border,
+    marginTop: 24,
+    gap: 8,
+  },
 });
